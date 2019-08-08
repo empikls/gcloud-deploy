da9e0fe0167248b64abb9be64402b7dcc3ad0746
Andrii Sh <ash@ansh-work>
2019-08-09 00:21:04 +0300

    add db configuration

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,6 +1,43 @@
+provider "google" {
+  credentials = "${file("~/.gcloud/gcpssproject-248009-54dc60693c76.json")}"
+  project     = "${var.project_name}"
+  region      = "${var.location}"
+}
+
+resource "google_sql_database_instance" "postgres" {
+  name = "${var.database_instance_name}"
+  database_version = "POSTGRES_9_6"
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+
+resource "google_sql_database" "database-prod" {
+  name      = "prod-db"
+  instance  = "${google_sql_database_instance.postgres.name}"
+}
+
+resource "google_sql_database" "database-test" {
+  name      = "test-db"
+  instance  = "${google_sql_database_instance.postgres.name}"
+}
+
+resource "google_sql_user" "users-prod" {
+  name     = "postgres"
+  instance = "${google_sql_database_instance.postgres.name}"
+  password = "${var.database_prod_user_pass}"
+}
+
+resource "google_sql_user" "users-test" {
+  name     = "postgres-test"
+  instance = "${google_sql_database_instance.postgres.name}"
+  password = "${var.database_test_user_pass}"
+}
+
 resource "google_container_cluster" "default" {
-  name        = "${var.name}"
-  project     = "${var.project}"
+  name        = "${var.cluster_name}"
+  project     = "${var.project_name}"
   description = "Demo GKE Cluster"
   location    = "${var.location}"
 
@@ -18,8 +55,8 @@ resource "google_container_cluster" "default" {
 }
 
 resource "google_container_node_pool" "default" {
-  name       = "${var.name}-node-pool"
-  project     = "${var.project}"
+  name       = "${var.cluster_name}-node-pool"
+  project     = "${var.project_name}"
   location   = "${var.location}"
   cluster    = "${google_container_cluster.default.name}"
   node_count = 1
